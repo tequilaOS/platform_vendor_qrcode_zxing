bananadroid/android_vendor_qrcode_zxing

Change package name for vendorsupport

zxing-android-embedded/src/com/google/zxing/client/android/BeepManager.java
@@ -27,6 +27,7 @@ import android.os.Build;
 import android.os.Vibrator;
 import android.util.Log;
 
+import com.ssos.support.R;
 import java.io.IOException;
 
 /**

zxing-android-embedded/src/com/journeyapps/barcodescanner/BarcodeView.java
@@ -7,7 +7,7 @@ import android.util.AttributeSet;
 
 import com.google.zxing.DecodeHintType;
 import com.google.zxing.ResultPoint;
-import com.google.zxing.client.android.R;
+import com.ssos.support.R;
 
 import java.util.HashMap;
 import java.util.List;

zxing-android-embedded/src/com/journeyapps/barcodescanner/CameraPreview.java
@@ -21,7 +21,7 @@ import android.view.TextureView;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 
-import com.google.zxing.client.android.R;
+import com.ssos.support.R;
 import com.journeyapps.barcodescanner.camera.CameraInstance;
 import com.journeyapps.barcodescanner.camera.CameraParametersCallback;
 import com.journeyapps.barcodescanner.camera.CameraSettings;

zxing-android-embedded/src/com/journeyapps/barcodescanner/CaptureManager.java
@@ -26,7 +26,7 @@ import com.google.zxing.ResultPoint;
 import com.google.zxing.client.android.BeepManager;
 import com.google.zxing.client.android.InactivityTimer;
 import com.google.zxing.client.android.Intents;
-import com.google.zxing.client.android.R;
+import com.ssos.support.R;
 
 import java.io.File;
 import java.io.FileOutputStream;

zxing-android-embedded/src/com/journeyapps/barcodescanner/DecoderThread.java
@@ -10,7 +10,7 @@ import android.util.Log;
 import com.google.zxing.LuminanceSource;
 import com.google.zxing.Result;
 import com.google.zxing.ResultPoint;
-import com.google.zxing.client.android.R;
+import com.ssos.support.R;
 import com.journeyapps.barcodescanner.camera.CameraInstance;
 import com.journeyapps.barcodescanner.camera.PreviewCallback;
 

zxing-android-embedded/src/com/journeyapps/barcodescanner/DecoratedBarcodeView.java
@@ -15,7 +15,7 @@ import com.google.zxing.ResultPoint;
 import com.google.zxing.client.android.DecodeFormatManager;
 import com.google.zxing.client.android.DecodeHintManager;
 import com.google.zxing.client.android.Intents;
-import com.google.zxing.client.android.R;
+import com.ssos.support.R;
 import com.journeyapps.barcodescanner.camera.CameraParametersCallback;
 import com.journeyapps.barcodescanner.camera.CameraSettings;
 

zxing-android-embedded/src/com/journeyapps/barcodescanner/ViewfinderView.java
@@ -27,7 +27,7 @@ import android.util.AttributeSet;
 import android.view.View;
 
 import com.google.zxing.ResultPoint;
-import com.google.zxing.client.android.R;
+import com.ssos.support.R;
 
 import java.util.ArrayList;
 import java.util.List;

zxing-android-embedded/src/com/journeyapps/barcodescanner/camera/CameraInstance.java
@@ -5,7 +5,7 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.SurfaceHolder;
 
-import com.google.zxing.client.android.R;
+import com.ssos.support.R;
 import com.journeyapps.barcodescanner.Size;
 import com.journeyapps.barcodescanner.Util;
 

tequilaOS/platform_vendor_qrcode_zxing

zxing-android-embedded/src/com/google/zxing/client/android/BeepManager.java
@@ -27,6 +27,7 @@ import android.os.Build;
 import android.os.Vibrator;
 import android.util.Log;
 
+import com.tequila.support.R;
 import java.io.IOException;
 
 /**

zxing-android-embedded/src/com/journeyapps/barcodescanner/BarcodeView.java
@@ -7,7 +7,7 @@ import android.util.AttributeSet;
 
 import com.google.zxing.DecodeHintType;
 import com.google.zxing.ResultPoint;
-import com.google.zxing.client.android.R;
+import com.tequila.support.R;
 
 import java.util.HashMap;
 import java.util.List;

zxing-android-embedded/src/com/journeyapps/barcodescanner/CameraPreview.java
@@ -21,7 +21,7 @@ import android.view.TextureView;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 
-import com.google.zxing.client.android.R;
+import com.tequila.support.R;
 import com.journeyapps.barcodescanner.camera.CameraInstance;
 import com.journeyapps.barcodescanner.camera.CameraParametersCallback;
 import com.journeyapps.barcodescanner.camera.CameraSettings;

zxing-android-embedded/src/com/journeyapps/barcodescanner/CaptureManager.java
@@ -26,7 +26,7 @@ import com.google.zxing.ResultPoint;
 import com.google.zxing.client.android.BeepManager;
 import com.google.zxing.client.android.InactivityTimer;
 import com.google.zxing.client.android.Intents;
-import com.google.zxing.client.android.R;
+import com.tequila.support.R;
 
 import java.io.File;
 import java.io.FileOutputStream;

zxing-android-embedded/src/com/journeyapps/barcodescanner/DecoderThread.java
@@ -10,7 +10,7 @@ import android.util.Log;
 import com.google.zxing.LuminanceSource;
 import com.google.zxing.Result;
 import com.google.zxing.ResultPoint;
-import com.google.zxing.client.android.R;
+import com.tequila.support.R;
 import com.journeyapps.barcodescanner.camera.CameraInstance;
 import com.journeyapps.barcodescanner.camera.PreviewCallback;
 

zxing-android-embedded/src/com/journeyapps/barcodescanner/DecoratedBarcodeView.java
@@ -15,7 +15,7 @@ import com.google.zxing.ResultPoint;
 import com.google.zxing.client.android.DecodeFormatManager;
 import com.google.zxing.client.android.DecodeHintManager;
 import com.google.zxing.client.android.Intents;
-import com.google.zxing.client.android.R;
+import com.tequila.support.R;
 import com.journeyapps.barcodescanner.camera.CameraParametersCallback;
 import com.journeyapps.barcodescanner.camera.CameraSettings;
 

zxing-android-embedded/src/com/journeyapps/barcodescanner/ViewfinderView.java
@@ -27,7 +27,7 @@ import android.util.AttributeSet;
 import android.view.View;
 
 import com.google.zxing.ResultPoint;
-import com.google.zxing.client.android.R;
+import com.tequila.support.R;
 
 import java.util.ArrayList;
 import java.util.List;

zxing-android-embedded/src/com/journeyapps/barcodescanner/camera/CameraInstance.java
@@ -5,7 +5,7 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.SurfaceHolder;
 
-import com.google.zxing.client.android.R;
+import com.tequila.support.R;
 import com.journeyapps.barcodescanner.Size;
 import com.journeyapps.barcodescanner.Util;
 